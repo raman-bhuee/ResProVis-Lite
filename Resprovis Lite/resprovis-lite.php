7a7b0094a196864415f60ae1b7d7522f95a92e9c
atsewa <99194154+atsewa@users.noreply.github.com>
2022-02-07 20:32:07 +0530

    Initial Check-in

---
+++ Resprovis Lite/resprovis-lite.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Plugin Name:       ResProVis Lite
+ * Plugin URI:
+ * Description:       ResProVis Lite  is a best plugin for additional functionality in Woocommerce Shop page. You can restrict  the visibility of items from the shop page according the Weekdays slots.
+ * Version:           1.0.0
+ * Requires at least: 5.3
+ * Author:            (@Sewa Developer Team)
+ * Author URI:        https://www.atsewa.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       resprovis
+ * Domain Path:       /languages
+ **/
+/*
+ * Exit if accessed directly
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+/*
+ * Defines a named
+ */
+define('RESPROVIS_TEXTDOMAIN', 'resprovis');
+define('RESPROVIS_DIR', plugin_dir_url(__FILE__));
+// Including all dependent files of the resprovis.php
+include("resprovis-lite_database.php");
+include_once("resprovis-lite_slotindex.php");
+include_once("resprovis-lite_productfiltration.php");
+
+
+// Calling wordpress activation hook for creating plugin tables in database when the plugin get installed.
+
+register_activation_hook(__FILE__, 'weekdays_alloc');
+register_activation_hook(__FILE__, 'weekdays');
+register_activation_hook(__FILE__, 'weekdays_data');
